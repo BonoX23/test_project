dc6e425ce759d0640cbfef19e44c4b1f20ddae7f
Ricardo Bono <83359117+BonoX23@users.noreply.github.com>
2022-12-01 19:12:59 -0300

    Aplicado o conceito AAA

--- alura.estacionamento-aula01/Alura.Estacionamento.Testes/VeiculosTestes.cs
+++ alura.estacionamento-aula01/Alura.Estacionamento.Testes/VeiculosTestes.cs
@@ -9,13 +9,28 @@ namespace Alura.Estacionamento.Testes
         [Fact]
         public void TestaVeiculoAcelerar()
         {
-            var veiculo = new Veiculo();
+
             
+            // Arrange é a preparação do ambiente
+            var veiculo = new Veiculo();
+
+            // Act é metodo que queremos testar no caso Acelerar() e Frear()
             veiculo.Acelerar(10);
 
-            //Verificação é feito com Assert
-            // Abaixo nos esperamos que o resultado seja 100 e nos iredmos verificar de veiculo.VelocidadeAtual
+            // Assert é para verificar o resultado obtido da verificaçao daquele método
+            // Abaixo colocamos no primeiro argumento o resultado que esperamos ou seja 100 e nos iredmos verificar de veiculo.VelocidadeAtual
             Assert.Equal(100, veiculo.VelocidadeAtual);
+
+        }
+
+        [Fact]
+
+        public void TestaVeiculoFrear()
+        {
+
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
 
         }
     }
