7cd85b818634816f78b899eb1e4db68b4b2ad5a8
Ricardo Bono <83359117+BonoX23@users.noreply.github.com>
2022-12-01 20:58:20 -0300

    Com vários dados

--- alura.estacionamento-aula01/Alura.Estacionamento.Testes/PatioTestes.cs
+++ alura.estacionamento-aula01/Alura.Estacionamento.Testes/PatioTestes.cs
@@ -1,7 +1,9 @@
-﻿using Alura.Estacionamento.Modelos;
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -20,7 +22,7 @@ namespace Alura.Estacionamento.Testes
             var veiculo = new Veiculo();
 
             veiculo.Proprietario = "André Silva";
-            veiculo.Tipo = Alura.Estacionamento.Modelos.TipoVeiculo.Automovel;
+            veiculo.Tipo = TipoVeiculo.Automovel;
             veiculo.Cor = "Verde";
             veiculo.Modelo = "Fusca";
             veiculo.Placa = "asd-9999";
@@ -35,6 +37,40 @@ namespace Alura.Estacionamento.Testes
             //Assert
             Assert.Equal(2, faturamento);
 
+        }
+
+
+
+        // Com Theory é possivel testar com um volume maior de dados 
+
+        [Theory]
+        [InlineData("André Silva", "ASD-1498", "Preto", "Gol")]
+        [InlineData("Jose Silva", "POL-9242", "Cinza", "Fusca")]
+        [InlineData("Maria Silva", "GDR-6524", "Azul", "Opala")]
+        [InlineData("Pedro Silva", "GDR-0101", "Azul", "Corsa")]
+        public void ValidaFaturametoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //Arrange (preparo do cenário)
+
+            var estacionamento = new Patio();
+
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
         }
     }
 }
